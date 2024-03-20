7709a481172a3d6b862b4951262353b6d08d0112
David Anyatonwu <davidanyatonwu@gmail.com>
2024-03-20 20:44:20 +0100

    fix

--- src/components/ConnectWallet.tsx
+++ src/components/ConnectWallet.tsx
@@ -2,6 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 import { AppConfig, showConnect, UserSession } from "@stacks/connect";
+import { Button } from "./ui/button";
+import { Bell } from "lucide-react";
+import Image from "next/image";
 
 const appConfig = new AppConfig(["store_write", "publish_data"]);
 
@@ -29,22 +32,31 @@ const ConnectWallet = () => {
   const [mounted, setMounted] = useState(false);
   useEffect(() => setMounted(true), []);
 
-  if (mounted && userSession.isUserSignedIn()) {
-    return (
-      <div className="Container">
-        <button className="Connect" onClick={disconnect}>
-          Disconnect Wallet
-        </button>
-        <p>mainnet: {userSession.loadUserData().profile.stxAddress.mainnet}</p>
-        <p>testnet: {userSession.loadUserData().profile.stxAddress.testnet}</p>
+  const walletInfo = mounted && userSession.isUserSignedIn() ? (
+    <>
+      <Button variant="destructive" color="red" className="mr-4" onClick={disconnect}>
+        Disconnect Wallet
+      </Button>
+      <div className="text-sm">
+        <p>Mainnet: <span className="font-semibold">{userSession.loadUserData().profile.stxAddress.mainnet}</span></p>
+        <p>Testnet: <span className="font-semibold">{userSession.loadUserData().profile.stxAddress.testnet}</span></p>
       </div>
-    );
-  }
+    </>
+  ) : (
+    <Button variant="default" color="blue" onClick={authenticate}>
+      Connect Wallet
+    </Button>
+  );
 
   return (
-    <button className="Connect" onClick={authenticate}>
-      Connect Wallet
-    </button>
+    <div className="flex justify-between items-center bg-slate-800 p-4">
+
+      <div className="flex items-center">
+        <Button variant="ghost" className="mr-4">
+        </Button>
+        {walletInfo}
+      </div>
+    </div>
   );
 };
 
